48d5d8ec99da3ba250ff12ea6ffce1b4c7a69424
Jeremy R. Fishman <jeremy.r.fishman@gmail.com>
2011-03-26 11:29:13 -0700

    updated copyright headers, added LICENSE

--- src/polyadics.h
+++ src/polyadics.h
@@ -1,22 +1,19 @@
 
-/* 
-** This file is part of PerTwisted, a persistant storage framework
-** tailored around the Twisted asynchronous network programming framework,
-** utilizing the Berkeley Database, Cononical's Storm ORM, implementations
-** of various DHTs and hopefully much more.
+/*
+** This file is part of polyadicts - addicted to data encapsulation.
 **
-** Pertwisted is free software: you can redistribute it and/or modify
+** Polyadicts is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation, either version 3 of the License, or
 ** (at your option) any later version.
 **
-** PerTwisted is distributed in the hope that it will be useful,
+** Polyadicts is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
 **
 ** You should have received a copy of the GNU General Public License
-** and the GNU Lesser Public License along with PerTwisted.  If not, see
+** and the GNU Lesser Public License along with polyadicts.  If not, see
 ** <http://www.gnu.org/licenses/>.
 */
 
