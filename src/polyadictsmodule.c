4f4c23c73da1fb5d90795a999d7093da191507cb
Jeremy R. Fishman <jeremy.r.fishman@gmail.com>
2015-10-15 13:41:22 -0400

    implement polyadicts.ntuple()

--- src/polyadictsmodule.c
+++ src/polyadictsmodule.c
@@ -19,9 +19,118 @@
 
 #include "polyadictsmodule.h"
 #include "polyadicobjects.h"
+#include "ntuple.h"
+
+static PyObject*
+_ntuple_frombuffer(PyObject *src)
+{
+    Py_buffer view;
+    size_t rank, size, i;
+    uint64_t *info;
+    PyObject *ret;
+
+    ret = NULL;
+    if (0 == PyObject_GetBuffer(src, &view, PyBUF_SIMPLE)) {
+        if (ntuple_rank(view.buf, view.len, &rank)) {
+            info = malloc(rank * sizeof(uint64_t));
+            if (info) {
+                size = ntuple_load(view.buf, view.len, rank, info);
+                if (size) {
+                    if (size == view.len) {
+                        ret = PyTuple_New(rank);
+                        if (ret) {
+                            for (i = 0; i < rank; i++) {
+                                PyTuple_SET_ITEM(ret, i, PyLong_FromUnsignedLongLong(info[i]));
+                            }
+                        }
+                    } else {
+                        PyErr_SetString(PyExc_ValueError, "buffer contains trailing data");
+                    }
+                }
+                free(info);
+            }
+        }
+    }
+    if (!ret && !PyErr_Occurred()) {
+        PyErr_SetFromErrno(PyExc_ValueError);
+    }
+    return ret;
+}
+
+static PyObject *
+_ntuple_fromsequence(PyObject *src)
+{
+    uint64_t *info;
+    size_t i, rank, size;
+    void *data;
+    PyObject *ret, *obj;
+
+    src = PySequence_Fast(src, "expected a sequence of natural numbers");
+    if (!src) {
+        return NULL;
+    }
+
+    rank = PySequence_Fast_GET_SIZE(src);
+    if (rank == 0) {
+        Py_DECREF(src);
+        return PyBytes_FromStringAndSize("\x00", 1);
+    }
+
+    ret = NULL;
+    info = malloc(rank * sizeof(uint64_t));
+    if (info) {
+        for (i = 0; i < rank; i++) {
+            obj = PySequence_Fast_GET_ITEM(src, i);
+            info[i] = PyLong_AsUnsignedLongLong(obj);
+            if (PyErr_Occurred()) {
+                break;
+            }
+        }
+        if (i == rank) {
+            size = ntuple_size(rank, info);
+            if (size) {
+                data = malloc(size);
+                if (data) {
+                    ntuple_pack(rank, info, data, size);
+                    ret = PyBytes_FromStringAndSize(data, size);
+                    free(data);
+                }
+            }
+        }
+        free(info);
+    }
+    Py_DECREF(src);
+    return ret;
+}
+
+static PyObject *
+polyadicts_ntuple(PyObject *self, PyObject *args)
+{
+    Py_ssize_t rank;
+    PyObject *ret, *arg;
+
+    ret = NULL;
+    rank = PyTuple_GET_SIZE(args);
+    if (rank == 1) {
+        arg = PyTuple_GET_ITEM(args, 0);
+        if (PyObject_CheckBuffer(arg)) {
+            ret = _ntuple_frombuffer(arg);
+        } else if (PySequence_Check(arg)) {
+            ret = _ntuple_fromsequence(arg);
+        } else {
+            ret = _ntuple_fromsequence(args);
+        }
+    } else {
+        ret = _ntuple_fromsequence(args);
+    }
+    return ret;
+}
 
 /* polyadicts module method defition */
 static PyMethodDef polyadicts_methods[] = {
+    {"ntuple", polyadicts_ntuple, METH_VARARGS,
+        "Pack or load a sequence of natural numbers"},
+
     {NULL} // Sentinel
 };
 
